4227c2d0779baa41d05196a18fef2bb8552a0d46
YuehChuan <schwarmcyc@gmail.com>
2025-08-07 08:19:28 +0800

    add stl ply model

---
+++ data/script/stlmmToM.py
@@ -0,0 +1,13 @@
+import trimesh
+
+# 載入 STL
+mesh = trimesh.load("2-1.STL")
+
+
+# 將頂點從 mm → m
+mesh.vertices *= 0.001
+
+# 儲存為新 STL
+mesh.export("2-1m.stl")
+print("Converted model saved to 2- m.stl")
+
